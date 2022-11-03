74618db263f825404f8d9cda47717deef00bbf06
Muhammad Fahlevi <muhammad.fahlevi@gdn-commerce.com>
2022-11-03 23:16:38 +0700

    Initialization project

---
+++ src/main/java/com/backend/lakik/LakikApplication.java
@@ -0,0 +1,13 @@
+package com.backend.lakik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/lakik/LakikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lakik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
