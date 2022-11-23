231cde2c56fd2b6dcbcf2b0dd37aefbd0ca6f795
Salman Marsha <salman.marsha@munalively.com>
2022-11-23 21:10:11 +0700

    fix: handle bugs and adjust error messages

--- src/main/java/com/backend/lakik/security/WebSecurityConfig.java
+++ src/main/java/com/backend/lakik/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.backend.lakik.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.backend.lakik.security.filter.AuthenticationFilter;
 import com.backend.lakik.security.filter.AuthorizationFilter;
@@ -33,4 +40,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
   }
   
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+    corsConfiguration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000"
+    ));
+    corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+    corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+    corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+    corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+    corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return source;
+  }
+
 }
